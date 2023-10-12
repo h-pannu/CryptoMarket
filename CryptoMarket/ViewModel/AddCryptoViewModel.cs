15e6c9a39e52472a5c47a46caec354a8bc84c4db
Harpreet Pannu <er.harpreetpannu@live.com>
2023-10-12 00:16:09 -0700

    Logic to Add Crypto using RelayCommand

---
+++ CryptoMarket/ViewModel/AddCryptoViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CryptoMarket.Commands;
+using CryptoMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CryptoMarket.ViewModel
+{
+    public class AddCryptoViewModel
+    {
+        public ICommand AddCryptoCommand { get; set; }
+        public string? _Symbol { get; set; }
+        public string? _Name { get; set; }
+        public double? _CurrentPrice { get; set; }
+        public double? _MarketCap { get; set; }
+
+        public AddCryptoViewModel()
+        {
+            AddCryptoCommand = new RelayCommand(AddCrypto, CanAddCrypto);
+        }
+
+        private void AddCrypto(object obj)
+        {
+            CryptoManager.AddCrypto(new Crypto() { Symbol = _Symbol, Name = _Name, CurrentPrice = _CurrentPrice, MarketCap = _MarketCap });
+        }
+
+        private bool CanAddCrypto(object obj)
+        {
+            return true;
+        }
+    }
+}
