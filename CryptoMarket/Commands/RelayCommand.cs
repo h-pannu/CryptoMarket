fa968dce8fc2e636ca8cb32f1c8865d06fec71a4
Harpreet Pannu <er.harpreetpannu@live.com>
2023-10-11 23:35:32 -0700

    Implemnetd ICommand interface in newly added RelayCommand class. implemenetd logic for CanExecute and Execute method.

---
+++ CryptoMarket/Commands/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CryptoMarket.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> _Excute { get; set; }
+        private Predicate<object> _CanExcute { get; set; }
+        public RelayCommand(Action<object> ExcuteMethod, Predicate<object> CanExcuteMethod)
+        {
+            _Excute = ExcuteMethod;
+            _CanExcute = CanExcuteMethod;
+        }
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            //throw new NotImplementedException();
+            return _CanExcute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            //throw new NotImplementedException();
+            _Excute(parameter);
+        }
+    }
+}
